70cb97b03b41ba9b87152829d56f846951ea3e34
borellaaaa <rafaelborella49@gmail.com>
2026-02-16 22:02:31 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,60 @@
+export const materiasDB = {
+    "HISTÓRIA": {
+        "INTRODUÇÃO": ["Linha do tempo", "Teoria da História"],
+        "PRÉ-HISTÓRIA": ["Paleolítico", "Neolítico", "Idade dos Metais"],
+        "IDADE ANTIGA": ["Egito", "Mesopotâmia", "Hebreus/Fenícios/Persas", "Grécia Antiga", "Roma Antiga"],
+        "IDADE MÉDIA": ["Império Bizantino", "Islamismo", "Reinos Bárbaros", "Feudalismo", "Baixa Idade Média (Cruzadas)"],
+        "IDADE MODERNA": ["Renascimento Cultural", "Absolutismo", "Mercantilismo", "Reforma Protestante", "Iluminismo", "Expansão Marítima"],
+        "IDADE CONTEMPORÂNEA": ["Revolução Francesa", "Era Napoleônica", "Revolução Industrial", "1ª Guerra Mundial", "Revolução Russa", "Crise de 29 e Nazifascismo", "2ª Guerra Mundial", "Guerra Fria", "Nova Ordem Mundial"],
+        "BRASIL COLÔNIA": ["Período Pré-Colonial", "Administração Colonial", "Economia Açucareira", "Mineração", "Revoltas Nativistas", "Família Real no Brasil"],
+        "BRASIL IMPÉRIO": ["Independência", "Primeiro Reinado", "Período Regencial", "Segundo Reinado"],
+        "BRASIL REPÚBLICA": ["República da Espada", "República Oligárquica", "Era Vargas", "República Populista", "Ditadura Militar", "Nova República"]
+    },
+    "GEOGRAFIA": {
+        "FÍSICA": ["Cartografia", "Geologia", "Relevo", "Clima", "Vegetação", "Hidrografia"],
+        "HUMANA": ["População (Demografia)", "Urbanização", "Migrações", "Espaço Agrário"],
+        "ECONÔMICA": ["Indústria", "Energia", "Transportes", "Comércio"],
+        "GEOPOLÍTICA": ["Globalização", "Blocos Econômicos", "Conflitos Atuais", "América Latina", "África", "Ásia", "Europa"],
+        "BRASIL": ["Regiões do Brasil", "Matriz Energética Brasileira", "Agropecuária Brasileira", "Impactos Ambientais"]
+    },
+    "BIOLOGIA": {
+        "BIOQUÍMICA": ["Água e Sais", "Carboidratos", "Lipídios", "Proteínas", "Ácidos Nucleicos"],
+        "CITOLOGIA": ["Membrana", "Citoplasma", "Núcleo", "Divisão Celular", "Metabolismo Energético"],
+        "GENÉTICA": ["Mendelismo", "Grupos Sanguíneos", "Biotecnologia"],
+        "EVOLUÇÃO": ["Teorias Evolutivas", "Especiação"],
+        "ECOLOGIA": ["Cadeias Alimentares", "Ciclos Biogeoquímicos", "Relações Ecológicas", "Poluição"],
+        "SERES VIVOS": ["Vírus", "Reino Monera", "Reino Protista", "Reino Fungi", "Reino Plantae (Botânica)", "Reino Animalia (Zoologia)"],
+        "FISIOLOGIA HUMANA": ["Digestão", "Respiração", "Circulação", "Excreção", "Nervoso/Endócrino", "Reprodução"]
+    },
+    "QUÍMICA": {
+        "GERAL": ["Modelos Atômicos", "Tabela Periódica", "Ligações Químicas", "Funções Inorgânicas", "Reações Químicas"],
+        "FÍSICO-QUÍMICA": ["Soluções", "Termoquímica", "Cinética", "Equilíbrio Químico", "Eletroquímica"],
+        "ORGÂNICA": ["Cadeias Carbônicas", "Hidrocarbonetos", "Funções Oxigenadas", "Funções Nitrogenadas", "Reações Orgânicas"]
+    },
+    "FÍSICA": {
+        "MECÂNICA": ["Cinemática", "Leis de Newton", "Trabalho e Energia", "Impulso e Qtd de Movimento", "Gravitação", "Hidrostática"],
+        "TERMOLOGIA": ["Termometria", "Calorimetria", "Termodinâmica", "Gases"],
+        "ÓPTICA": ["Espelhos", "Lentes", "Ondulatória", "Acústica"],
+        "ELETRICIDADE": ["Eletrostática", "Eletrodinâmica", "Eletromagnetismo"]
+    },
+    "MATEMÁTICA": {
+        "BÁSICA": ["Conjuntos", "Frações/Decimais", "Potência/Raiz", "Regra de Três/Porcentagem"],
+        "FUNÇÕES": ["1º Grau", "2º Grau", "Exponencial", "Logarítmica"],
+        "GEOMETRIA PLANA": ["Triângulos", "Polígonos", "Círculo"],
+        "GEOMETRIA ESPACIAL": ["Poliedros", "Corpos Redondos"],
+        "ESTATÍSTICA": ["Média/Moda/Mediana", "Análise Combinatória", "Probabilidade"],
+        "TRIGONOMETRIA": ["Triângulo Retângulo", "Ciclo Trigonométrico"]
+    },
+    "PORTUGUÊS": {
+        "GRAMÁTICA": ["Morfologia", "Sintaxe", "Semântica", "Ortografia"],
+        "LITERATURA": ["Trovadorismo ao Classicismo", "Barroco e Arcadismo", "Romantismo", "Realismo/Naturalismo/Parnasianismo", "Simbolismo/Pré-Modernismo", "Modernismo"],
+        "INTERPRETAÇÃO": ["Figuras de Linguagem", "Gêneros Textuais", "Funções da Linguagem"]
+    },
+    "INGLÊS/ESPANHOL": {
+        "GERAL": ["Interpretação de Texto", "Vocabulário", "Gramática Básica"]
+    },
+    "FILOSOFIA/SOCIOLOGIA": {
+        "FILOSOFIA": ["Antiga", "Medieval", "Moderna", "Contemporânea"],
+        "SOCIOLOGIA": ["Clássica (Marx/Weber/Durkheim)", "Trabalho", "Cultura", "Política"]
+    }
+};
